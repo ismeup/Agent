98d2c5511c27cd44fdc2c7965b209cc503e2b42b
r0ck3r <r0ck3r@gmail.com>
2026-08-26 15:22:08 +0300

    #27 - incremented agent version

--- src/agent/config.py
+++ src/agent/config.py
@@ -1,6 +1,6 @@
 import os
 
-AGENT_VERSION = 1
+AGENT_VERSION = 2
 KEY_FILE = os.environ.get("AGENT_KEY_PATH") or "data/identity.key"
 ISMEUP_HOST = os.environ.get("ISMEUP_HOST") or "ismeup.net"
 ISMEUP_PORT = os.environ.get("ISMEUP_PORT") or "8787"
